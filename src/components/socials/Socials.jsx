4d0efcab3ad60a27609d9c2631160de7106d4409
smirnoff404 <smirnof528@gmail.com>
2024-07-22 12:34:04 +0300

    gallery-scale-fix

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -9,8 +9,8 @@ const Socials = ({isAdaptive}) => {
     return (
         <div className={`${styles.socials} ${handleStyle(isAdaptive)}`}>
             {/* <a href='https://www.viber.com/ru/' target='_blank'><img src='/images/viber.png'/></a> */}
-            <a href='https://api.whatsapp.com/send?phone=79107647349' target='_blank'><img src='/images/whatsapp.png'/></a>
-            <a href='https://t.me/plus_friend' target='_blank'><img src='/images/telegram.png'/></a>
+            <a href='https://api.whatsapp.com/send?phone=79107647349' target='_blank'><img className={styles.img} src='/images/whatsapp.png'/></a>
+            <a href='https://t.me/plus_friend' target='_blank'><img className={styles.img} src='/images/telegram.png'/></a>
         </div>
     )
 }
